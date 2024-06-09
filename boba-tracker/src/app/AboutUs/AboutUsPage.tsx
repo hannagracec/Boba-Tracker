bc23422728a227157a4e5f1a786c5b61b0fe4d87
hannagracec <hannacho847@gmail.com>
2024-06-08 22:51:06 -0600

    Organized code for increased readability

--- boba-tracker/src/app/AboutUs/AboutUsPage.tsx
+++ boba-tracker/src/app/AboutUs/AboutUsPage.tsx
@@ -17,10 +17,18 @@ const browse_alt = "Search icon";
 const heart_alt = "Heart icon";
 const star_alt = "Shooting star icon";
 
+const ABOUT_US_CONTENT_STYLES = "flex flex-col items-center p-4";
+
+const SIGNMEUP_BUTTON_STYLES = 
+    "flex items-center bg-pastel-pink py-2 px-4 w-full max-w-xs justify-center border-2 border-black-ish rounded-3xl shadow-b";
+const SIGNMEUP_PRESSED_BUTTON_STYLES = 
+    "focus:outline-black-ish transition-all duration-200 active:shadow-none active:translate-y-0.5 active:border-black-ish";
+const SIGNMEUP_BUTTON_TEXT_STYLES = "text-xl font-black text-black-ish";
+
 const AboutUsPage = () => {
   return(
     <div>
-      <div className="flex flex-col items-center p-4">
+      <div className={ABOUT_US_CONTENT_STYLES}>
         <div className={`text-5xl flex ${rubik_mono_one.className} mb-6`}>
           <h1>About</h1>
           <Image
@@ -38,8 +46,8 @@ const AboutUsPage = () => {
           <InfoTile icon={shooting_star_icon} text={rate_blurb} alt={star_alt} />
         </div>
         <Link href="/Login">
-          <button className="flex items-center bg-pastel-pink py-2 px-4 w-full max-w-xs justify-center border-2 border-black-ish rounded-3xl shadow-b focus:outline-black-ish transition-all duration-200 active:shadow-none active:translate-y-0.5 active:border-black-ish">
-            <p className="text-xl font-black text-black-ish">Sign me up!</p>
+          <button className={`${SIGNMEUP_BUTTON_STYLES} ${SIGNMEUP_PRESSED_BUTTON_STYLES}`}>
+            <p className={SIGNMEUP_BUTTON_TEXT_STYLES}>Sign me up!</p>
           </button>
         </Link>
       </div>
--- boba-tracker/src/components/LandingPage/GettingStarted.tsx
+++ boba-tracker/src/components/LandingPage/GettingStarted.tsx
@@ -5,13 +5,25 @@ import { rubik_mono_one } from "@/src/app/fonts";
 const solid_star = "/LandingPage/solid_star.svg";
 const circle_arrow = "LandingPage/circle_arrow.svg";
 
+const GET_STARTED_PAGE_STYLES = "w-full max-w-[400px]";
+const GET_STARTED_CONTENT_STYLES = "px-8 text-black-ish mb-16";
+
+const TITLE_SECTION_STYLES = "mt-10 text-5xl text-off-white";
+
 const LOGO_CONTAINER_STYLES =
   "bg-off-white size-[150px] border-2  border-black-ish rounded-[100%] flex shadow-b justify-center";
 
+const PHRASE_SUBHEADER_STYLES = "text-xl mt-6 mb-10 max-w-[300px] font-medium leading-[1.2]";
+const GET_STARTED_BUTTON_STYLES = 
+  "flex items-center bg-off-white py-2 w-full justify-center border-2 border-black-ish rounded-lg shadow-b";
+const GET_STARTED_PRESSED_BUTTON_STYLES = 
+  "focus:outline-black-ish transition-all duration-200 active:shadow-none active:translate-y-1 active:border-black-ish";
+const GET_STARTED_BUTTON_TEXT_STYLES = "text-xl font-black mr-2";
+
 const GettingStarted = () => {
   return (
-    <div className="w-full max-w-[400px]">
-      <div className="px-8 text-black-ish mb-16">
+    <div className={GET_STARTED_PAGE_STYLES}>
+      <div className={GET_STARTED_CONTENT_STYLES}>
         <div className="flex justify-center">
           <div className={LOGO_CONTAINER_STYLES}>
             <Image
@@ -22,7 +34,7 @@ const GettingStarted = () => {
             />
           </div>
         </div>
-        <div className="mt-10 text-5xl text-off-white">
+        <div className={TITLE_SECTION_STYLES}>
           <div className="flex">
             <h1 className={rubik_mono_one.className}>Boba</h1>
             <Image 
@@ -35,12 +47,12 @@ const GettingStarted = () => {
           </div>
           <h1 className={rubik_mono_one.className}>Tracker</h1>
         </div>
-        <h2 className="text-xl mt-6 mb-10 max-w-[300px] font-medium leading-[1.2]">
+        <h2 className={PHRASE_SUBHEADER_STYLES}>
           Start tracking your perfect boba blend at every store!
         </h2>
         <Link href="/AboutUs">
-          <button className="flex items-center bg-off-white py-2 w-full justify-center border-2 border-black-ish rounded-lg shadow-b focus:outline-black-ish transition-all duration-200 active:shadow-none active:translate-y-1 active:border-black-ish">
-            <p className="text-xl font-black mr-2">Get Started</p>
+          <button className={`${GET_STARTED_BUTTON_STYLES} ${GET_STARTED_PRESSED_BUTTON_STYLES}`}>
+            <p className={GET_STARTED_BUTTON_TEXT_STYLES}>Get Started</p>
             <Image 
               src={circle_arrow}
               height={30}

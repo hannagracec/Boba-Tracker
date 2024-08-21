571e7802fe75a1de8381dcb6d39db800a1394680
hannagracec <hannacho847@gmail.com>
2024-08-21 04:05:19 -0600

    clean up code slightly

--- boba-tracker/src/app/Dashboard/Browse/BrowseStoresPage.tsx
+++ boba-tracker/src/app/Dashboard/Browse/BrowseStoresPage.tsx
@@ -14,6 +14,12 @@ interface StoreData {
 
 const shop_icon = "/Dashboard/shop.svg";
 
+const BROWSE_STORES_CONTENT_STYLES = "p-4 text-black-ish w-full max-w-[600px] overflow-y-auto pb-[160px] bg-off-white";
+const BROWSE_STORES_HEADER_CONTAINER_STYLES = "bg-white-ish border-2 border-black-ish py-4 px-8 rounded-lg flex";
+const HEADER_ICON_CONTAINER_STYLES = 
+  "mr-4 bg-deep-coral rounded-full p-4 size-16 flex min-w-16 justify-center items-center border-2 border-black-ish";
+const HEADER_TEXT_STYLES = "text-[10px] leading-[1.2] font-medium max-w-[300px]";
+
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
@@ -47,15 +53,15 @@ const BrowseStoresPage = () => {
   };
 
   return (
-    <div className="p-4 text-black-ish w-full max-w-[600px] overflow-y-auto pb-[160px] bg-off-white">
+    <div className={BROWSE_STORES_CONTENT_STYLES}>
       <div className={selectedStore ? 'blur-[1px]' : ''}>
-        <div className="bg-white-ish border-2 border-black-ish py-4 px-8 rounded-lg flex">
-          <div className="mr-4 bg-deep-coral rounded-full p-4 size-16 flex min-w-16 justify-center items-center border-2 border-black-ish">
+        <div className={BROWSE_STORES_HEADER_CONTAINER_STYLES}>
+          <div className={HEADER_ICON_CONTAINER_STYLES}>
             <Image src={shop_icon} height={35} width={35} alt="Browse stores icon" />
           </div>
           <div>
             <h1 className="font-black text-xl">Browse Stores</h1>
-            <p className="text-[10px] leading-[1.2] font-medium max-w-[300px]">
+            <p className={HEADER_TEXT_STYLES}>
               Explore the stores listed below to browse their menu or select a drink to save
             </p>
           </div>
